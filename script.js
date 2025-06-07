fb0aa507bb7960b5f82016e9190ac0d481a70859
Lucas beno <lprb@cesar.school>
2025-06-07 12:19:53 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('open');
+});
